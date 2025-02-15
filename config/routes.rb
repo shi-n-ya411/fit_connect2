3b3812e5a6f29d90987f696f729d738855a9a1ce
EC2 Default User <ec2-user@ip-172-31-26-242.eu-north-1.compute.internal>
2025-02-15 06:52:38 +0000

    add-guest-sign-in

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,10 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
+  # ゲストログイン
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
 
   # ルートと静的ページ
   root to: 'homes#top'
